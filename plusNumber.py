3d8feb44838690b34b919631729fdc8582791149
Alex Bolaño Cervantes <alexander.bolano@192.168.0.4>
2024-03-19 15:24:08 -0500

    Test to conn repo

---
+++ plusNumber.py
@@ -0,0 +1,20 @@
+import random
+
+def sum(a: int, b: int) -> int:
+
+    """
+    This Function return the sumatory of 2 numbers.
+
+    Args:
+        a: integer.
+        b: integer.
+
+    Returns:
+        a plus b.
+    """
+
+    sum = a + b
+
+    return sum
+
+print(f"The result is {sum(2,5)}")
